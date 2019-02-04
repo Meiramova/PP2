f234cb4ae034ec3ec699f2c9d5b918636ffb1629
Meiramova <47049426+Meiramova@users.noreply.github.com>
2019-02-04 21:00:06 +0600

    Updated Task 2 with comments

--- WEEK 1/Task 2/Task 2/Program.cs
+++ WEEK 1/Task 2/Task 2/Program.cs
@@ -8,39 +8,39 @@ namespace Task_2
 {
     class Program
     {
-        class Student
+        class Student // создаю класс
         {
-            public string name;
+            public string name;// задаю ему свои переменные
             public string id;
             public int year = 0;
 
             public Student(string name, string id)
             {
-                this.name = name;
+                this.name = name; // создаю конструктор
                 this.id = id;
             }
 
             public void Print()
             {
-                Console.WriteLine("Name of student is " + name);
+                Console.WriteLine("Name of student is " + name); // создаю функцию для вывода
                 Console.WriteLine("Student's id " + id);
             }
 
             public void Print2()
             {
-                ++year;
-                Console.WriteLine("Student's increased year is " + year);
+                ++year; // создала инкремент(увеличивается)
+                Console.WriteLine("Student's increased year is " + year); // функция вывода 2 
             }
         }
         static void Main(string[] args)
         {
-            string s = Console.ReadLine();
-            string[] str = s.Split();
-            Student n1 = new Student(str[0], str[1]);
-            n1.year = int.Parse(str[2]);
-            n1.Print();
-            n1.Print2();
-            Console.ReadKey();
+            string s = Console.ReadLine(); // считываю строку
+            string[] str = s.Split(); // разделяю его на пробелы
+            Student n1 = new Student(str[0], str[1]); // первые 2 элемента и есть имя и id
+            n1.year = int.Parse(str[2]); // перевожу со стринга на инт через функцию парс
+            n1.Print();// вызываю функцию вывода
+            n1.Print2();//вызываю функцию вывода 2
+            Console.ReadKey();// чтобы терминал не закрывался
         }
     }
 }
