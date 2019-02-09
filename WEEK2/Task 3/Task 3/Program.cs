0e4f2d0b0955556fc2cd163e6cf9984d142c9703
Meiramova <ziya.meiramova@mail.ru>
2019-02-09 09:52:23 +0600

    week 2 added

---
+++ WEEK2/Task 3/Task 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_3
+{
+    class Program
+    {
+        public static void Ex1(int level)
+        {
+            for (int i = 0; i < level; i++)
+            {
+                Console.Write("       ");
+            }
+
+        }
+
+        public static void Ex2(DirectoryInfo der, int level)
+        {
+
+            foreach (FileInfo file in der.GetFiles())
+            {
+                Ex1(level);
+                Console.WriteLine(file.Name);
+
+            }
+
+            foreach (DirectoryInfo diru in der.GetDirectories())
+            {
+                Ex1(level);
+                Console.WriteLine(diru.Name);
+                Ex2(diru, level + 1);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo("C:/Users/Зия/Desktop/study");
+            Ex2(dir, 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WEEK2/Task 4/Task 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_4
+{
+    class Program
+    {
+        public static void f1()//метод чтобы создавать, кпировать и удалять файл
+        {
+            StreamWriter xaxa = new StreamWriter("C:/Users/Зия/Desktop/C#/uf.txt");//создаю файл
+            xaxa.WriteLine("Privet iz proshlogo");
+            xaxa.Close();
+            // есть другой способ создаать файл сразу заполнив его, через File.WriteAllText("C:/Users/Зия/Desktop/C#/uf.txt", "Privet iz proshlogo")
+            File.Copy("C:/Users/Зия/Desktop/C#/uf.txt", "C:/Users/Зия/Desktop/study/uf.txt");// копируем файл
+            File.Delete("C:/Users/Зия/Desktop/C#/uf.txt");// удаляем файл
+        }
+
+        public static void f2()// етод чтобы создать и переместить файл
+        {
+            Directory.CreateDirectory("C:/Users/Зия/Desktop/C#/XDXD");//создаем директорий
+            Directory.Move("C:/Users/Зия/Desktop/C#/XDXD", "C:/Users/Зия/Desktop/study/XDXD");// перемещаем директорий
+            //Directory.Delete("C:/Users/Зия/Desktop/C#/XDXD"); но мы не сможем его использовать после перемещения
+
+        }
+        static void Main(string[] args)
+        { //вызываем методы
+            f1();
+            f2();
+            Console.ReadKey();
+        }
+    }
+}
